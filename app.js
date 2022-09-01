74b4b8c3b9bcdc8ed03f3233fd23df9ca024b663
Sergio Fenoll <sergio@fenoll.be>
2022-09-01 22:51:10 +0200

    refactor: sync fields with decisionmakingFlow instead of case

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ import { app, errorHandler } from 'mu';
 import bodyParser from 'body-parser';
 
 import { reduceChangesets } from './lib/delta-util';
-import { caseFromSubjectUri } from './lib/util-queries';
-import { syncFieldsForCaseInGraph } from './lib/case-field-queries';
+import { decisionmakingFlowFromSubjectUri } from './lib/util-queries';
+import { syncFieldsForDecisionmakingFlowInGraph } from './lib/decisionmaking-flow-field-queries';
 
 app.post('/delta', bodyParser.json(), async (req, res) => {
   res.status(202).end();
   const deltas = req.body;
-  // could be cases or subcases
+  // could be decisionmakingFlow or subcases
   if (deltas.length) {
     console.debug(`Received deltas (${deltas.length} total)`);
   } else {
@@ -16,23 +16,23 @@ app.post('/delta', bodyParser.json(), async (req, res) => {
   }
 
   // Unique set of subject URI's from insert and delete with correct predicate (should be subcase)
-  // * note, these can still contain cases unless we apply a string filter on subject
+  // * note, these can still contain decisionmakingFlows unless we apply a string filter on subject
   const subjectUris = reduceChangesets(deltas);
 
-  // unique set of cases connected
-  const caseUris = new Set();
+  // unique set of decisionmakingFlows connected
+  const decisionmakingFlowUris = new Set();
 
   for (const subjectUri of subjectUris) {
-    const _case = await caseFromSubjectUri(subjectUri);
-    if (_case) {
-      caseUris.add(_case);
+    const decisionmakingFlow = await decisionmakingFlowFromSubjectUri(subjectUri);
+    if (decisionmakingFlow) {
+      decisionmakingFlowUris.add(decisionmakingFlow);
     }
   }
 
-  // process case by case, updating the collection on case
-  for (const caseUri of caseUris) {
-    await syncFieldsForCaseInGraph(caseUri);
-    caseUris.delete(caseUri);
+  // process resource by resource, updating the collection on decisionmakingFlow
+  for (const uri of decisionmakingFlowUris) {
+    await syncFieldsForDecisionmakingFlowInGraph(uri);
+    decisionmakingFlowUris.delete(uri);
   }
 });
 
